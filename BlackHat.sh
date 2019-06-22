66a382d4da0d909909a12ea5b0ec73266fea5083
BHC-3RR0R <51839125+BHC-FAKE@users.noreply.github.com>
2019-06-22 15:39:34 +0700

    Add files via upload

---
+++ BlackHat.sh
@@ -0,0 +1,122 @@
+#usr/bin/bash#
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo
+echo
+figlet INSTALL DOLO TUNGGU | lolcat
+clear
+echo
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+echo
+clear
+figlet WELCOME MY TOOLS😙😙😙 | lolcat
+echo $i"["$bi"•"$i"]"$me"────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"                BLACK HAT CYBER TEAM"$i"              |"
+echo $i" |"$pu"──────────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" FAKE  [BlackHatCyber]"$i"                   |"
+echo $i" |"$pu" PLANET :"$ku" MARS"$i"                                    |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 0858××××××××"$i"                  |"
+echo $i" |"$pu" Contack Gmail :"$ku" blackhatcyberteam@gmail.com"$i"      |"
+echo $i"["$bi"•"$i"]"$me"────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"─────────────────────────────────────────"
+echo $i"|"$me" 1"$i" |"$pu" TOOLS BRUTAL SMS WORK    "$i"|"$i"    |"$cy" J"$i" |"
+echo $i"|"$me" 2"$i" |"$pu" TOOLS PENAMBAH PINTAS    "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 3"$i" |"$pu" TOOLS YAHOO CLONING      "$i"|"$i"    |"$cy" H"$i" |"
+echo $i"|"$me" 4"$i" |"$pu" TOOLS BUAT DEFACER       "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me" 5"$i" |"$pu" TOOLS DARK-FB            "$i"|"$i"    |"$cy" T"$i" |"
+echo $i"────────────────────────────────"$i
+echo $i"|"$me" 6"$i" |"$pu" TOOLS DDOS               "$i"|"$i"    |"$cy" X"$i" |"
+echo $i"────────────────────────────────"$i
+echo $i"|"$me" 7"$i" |"$pu" EXIT/PROGRAMAN "
+echo $i"────────────────────────────────"$i
+echo
+echo $me"┌==="$bi"["$i"BlackHatCyber"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/BHC-FAKE/keyrun1
+cd keyrun1
+python2 runcut.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+
+fi
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash webdav77.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1g
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1g
+git clone  https://github.com/WahyuHack/wahyuddos
+cd wahyuddos
+python wahyuddos.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "E.X.I.T"|lolcat
+echo "\033[37;1mTerima Kasih Sudah Mengunakan Tools Ini:)"
+echo "\033[37;1mJangan Lupa Support Kami Thanks You"
+echo "\033[37;1mKalo Ada Kesalahan Bisa Chat"
+echo "\033[37;1mIG : action_hackers"
+echo "\033[37;1mGUNAKAN TOOLS INI DENGAN JAHAD :V"
+echo "\033[37;1mThanks To : BHC•O²N¦Mr.Rexx¦√⁩ - ⁨BHC•Mr.HanSIPᵀᴶᴳ⁩ - BHC•Hackeyst - BHC•Poker - BHC•Mr.Black.ID- All Member B.H.C"
+fi
